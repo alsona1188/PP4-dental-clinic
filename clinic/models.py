e193039329321d9a51a0c1476391428060e59408
Alsona Natsi <alsona.naci1188@gmail.com>
2024-06-05 22:36:49 +0000

    Creating the Dentist model with a many to many relationship with service

--- clinic/models.py
+++ clinic/models.py
@@ -5,12 +5,21 @@ from django.contrib.auth.models import User
 
 class Service(models.Model):
     # Service model for storing services data.
-    title = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200, unique=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField(default='')
 
     def __str__(self):
         return self.name
 
+class Dentist(models.Model):
+    # Dentist model for storing the dentist's data.
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    specialty = models.CharField(max_length=200)
+    services = models.ManyToManyField('Service', related_name='dentists')
+
+    def __str__(self):
+        return f'Dr. {self.first_name} {self.last_name}'
 
 
--- dental/settings.py
+++ dental/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8cm4li*4%o8^alr@cuga5411%tw**mm5z4(jn$z$%4nr=#8456'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-alsona1188-pp4dentalcli-7zttegzvmzb.ws-eu114.gitpod.io', '.herokuapp.com']
 
