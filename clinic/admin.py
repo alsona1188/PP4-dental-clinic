91e3a0930d2a16c5a742665b2e61201eb001240b
Alsona Natsi <alsona.naci1188@gmail.com>
2024-06-06 02:43:29 +0000

    Modifying the standard admin dashboard using Django methods and integrating the Summernote package.

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import Service, Dentist
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Service)
-admin.site.register(Dentist)
+
+@admin.register(Service)
+class ServiceAdmin(SummernoteModelAdmin):
+    list_display = ['name', 'price', 'description']
+    search_fields = ['name', 'price']
+    ordering = ['price'] 
+    summernote_fields = ('description',)
+    list_filter = ('price',)
+
+
+@admin.register(Dentist)
+class DentistAdmin(SummernoteModelAdmin):
+    list_display = ['first_name', 'last_name', 'specialty']
+    search_fields = ['specialty', 'first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
+    list_filter = ('specialty',)
