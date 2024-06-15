4680521a60fee4b97f699348a313fc0257089d17
Alsona Natsi <alsona.naci1188@gmail.com>
2024-06-15 02:13:35 +0000

    Creating the function appointment_edit to make possible the editing of the appointments

--- my_appointments/urls.py
+++ my_appointments/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = 'my_appointments'
 
 urlpatterns = [
+    path('appointment/<int:pk>/edit/', views.appointment_edit, name='appointment_edit'),
     
     path('my_appointments/', views.appointment_list, name='appointment_list'),
     
--- my_appointments/views.py
+++ my_appointments/views.py
@@ -1,6 +1,6 @@
-
-from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from appointment.forms import AppointmentForm
 from appointment.models import AppointmentRequest
 
@@ -9,3 +9,19 @@ from appointment.models import AppointmentRequest
 def appointment_list(request):
     appointments = AppointmentRequest.objects.filter(user=request.user)
     return render(request, 'my_appointments/appointment_list.html', {'appointments': appointments})
+
+
+@login_required
+def appointment_edit(request, pk):
+    appointment = get_object_or_404(AppointmentRequest, pk=pk, user=request.user)
+    if request.method == "POST":
+        appointment_form = AppointmentForm(request.POST, instance=appointment)
+        if appointment_form.is_valid():
+            appointment_form.save()
+            messages.add_message(request, messages.SUCCESS, 'Appointment updated successfully.')
+            return redirect('my_appointments:appointment_list')
+        else:
+            messages.add_message(request, messages.ERROR, 'Failed to update the appointment. Please check the form.')
+    else:
+        appointment_form = AppointmentForm(instance=appointment) 
+    return render(request, 'my_appointments/appointment_edit.html', {"appointment_form": appointment_form})
